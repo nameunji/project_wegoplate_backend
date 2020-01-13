25eb476045208bad933e671fee32eddb4950ab9c
ttwkr <ttwkor@naver.com>
2020-01-13 12:27:56 +0900

    JsonResponse 결과 문구 수정

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+from restaurant.models import(
+    Top_List, 
+    Topic,
+    Topic_Top_list, 
+    )
+
+class MainTopList(TestCase):
+    def setUp(self):
+        client = Client()
+        
+        Topic.objects.create(
+            id = 1,
+            title = '믿고 보는 맛집 리스트'
+        )
+
+        Top_List.objects.create(
+            id = 1,
+            title = '2020 제주 인기 맛집 TOP 60',
+            description = '제주의 인기 맛집만 쏙쏙 골라 모았다!!!',
+            image = 'https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/7zsdxmpu4kauzpk7.jpg'
+        )
+
+        Topic_Top_list.objects.create(
+            id = 1,
+            top_list_id = 1,
+            topic_id = 1
+        )
+    def tearDown(self):
+        Topic.objects.all().delete()
+        Top_List.objects.all().delete()
+        
+    def test_TopTopic(self):
+        client = Client()
+
+        response = client.get('/restaurant/topic/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                'title' : '믿고 보는 맛집 리스트',
+                'top_list' : [
+                    {
+                        'id' : 1,
+                        'title' : '2020 제주 인기 맛집 TOP 60',
+                        'description' : '제주의 인기 맛집만 쏙쏙 골라 모았다!!!',
+                        'image' : 'https://mp-seoul-image-production-s3.mangoplate.com/keyword_search/meta/pictures/7zsdxmpu4kauzpk7.jpg'
+                    }
+                ]
+            }
+        )
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from .models import Topic, Topic_Top_list
+
+class TopTopic(View):
+    def get(self, request, topic_id):
+
+        try:
+            topic_top_list = Topic_Top_list.objects.select_related('topic', 'top_list').filter(topic__id = topic_id)
+            topic_title = topic_top_list[0].topic.title
+
+            toplists = [{
+                'id' : toplist.top_list.id,
+                'title' : toplist.top_list.title,
+                'description' : toplist.top_list.description,
+                'image' : toplist.top_list.image
+            } for toplist in topic_top_list ]
+
+            return JsonResponse({'title' : topic_title, 'top_list' : toplists}, status=200)
+        except Topic.DoesNotExist:
+            return JsonResponse({'result' : 'DOES_NOT_EXIST_TOPIC'}, status = 400)
