eae54008265ea2b45b2ba46b141e56d96119d0cc
ttwkr <ttwkor@naver.com>
2020-01-13 19:15:22 +0900

    sign up 추가

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views      import SignUpView, NicknameCheckView
+from .views      import SignUpView, NicknameCheckView, SignInView, SocialKakaoView
 
 urlpatterns = [
     path('', SignUpView.as_view()),
-    path('/nickname', NicknameCheckView.as_view())
+    path('/signin', SignInView.as_view()),
+    path('/nickname', NicknameCheckView.as_view()),
+    path('/kakao', SocialKakaoView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ import re
 import jwt
 import json
 import bcrypt
+import requests
 
 from django.views               import View
 from django.db                  import IntegrityError
@@ -50,4 +51,54 @@ class SignUpView(View):
         except KeyError:
             return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
 
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            user = User.objects.get(email = data['email'])
 
+            if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                access_token = jwt.encode({'id' : user.id}, SECRET_KEY, algorithm='HS256')
+
+                return JsonResponse(
+                    {
+                        'access_token' : access_token.decode('utf-8'),
+                        'message' : "POSSIBLE"
+                    }, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'INVALID_KEYS'}, status=400)
+        
+        except ValueError:
+            return JsonResponse({'message' : 'INVALID_VALUE'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_USER'}, status=400)
+
+class SocialKakaoView(View):
+    def post(self, request):
+
+        kakao_token = request.headers["Authorization"]
+        if not kakao_token:
+            return JsonResponse({"MESSAGE" : "INVALID_KAKAO_TOKEN"}, status=400)
+
+        headers = {'Authorization' : f"Bearer {kakao_token}"}
+        url = "https://kapi.kakao.com/v2/user/me"
+        response = requests.get(url, headers = headers)
+        kakao_user = response.json()
+
+        if User.objects.filter(kakao = kakao_user['id']).exists():
+
+            user = User.objects.get(kakao = kakao_user['id'])
+            access_token = jwt.encode({'user_id' : user.id}, SECRET_KEY, algorithm='HS256')
+
+            return JsonResponse({'access_token' : access_token.decode('utf-8'),}, status=200)
+
+        else :
+            newUser = User.objects.create(
+                kakao = kakao_user['id'],
+                nick_name = kakao_user['properties']['nickname']
+            )
+
+            access_token = jwt.encode({'id' : newUser.id}, SECRET_KEY, algorithm='HS256')
+            return JsonResponse({'access_token': access_token.decode('utf-8')}, status=200)
