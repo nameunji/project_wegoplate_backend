78e56411e11e31afcfbf229a132ebe76df614264
nameunji <namji117@gmail.com>
2020-01-15 02:24:54 +0900

    add restaurant_detail_info_view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,18 +1,11 @@
 import json
 
-from django.http      import JsonResponse
+from .models          import *
+from user.models      import Review, Review_image, Review_Star
+
 from django.views     import View
 from django.db.models import Avg
-
-from .models import (
-        Topic, 
-        Topic_Top_list,
-        Restaurant_image, 
-        Restaurant, 
-        Restaurant_Info,
-        Restaurant_Tag
-    )
-from user.models import Review, Review_image, Review_Star
+from django.http      import JsonResponse, HttpResponse
 
 class TopTopic(View):
     def get(self, request, topic_id):
@@ -56,3 +49,51 @@ class DetailTopImage(View):
 
         return JsonResponse({'image' : list(image)})
 
+class RestaurantDetailInfoView(View):
+    def get(self, request, restaurant_id):
+        try:
+            restaurant = Restaurant.objects.select_related('price_range', 'food', 'location_city', 'location_state', 'location_road', 'holiday').prefetch_related('menu_set','restaurant_info_set').get(id=restaurant_id)
+
+            title_dict = { "parking":"주차", "number":"전화번호", "last_order":"마지막주문", "site":"웹 사이트", "breaktime":"쉬는시간", "opening_hours":"영업시간"}
+
+            result = []
+            # address
+            result.append({
+                "title"   : "주소",
+                "content" : ['{} {} {} {}'.format(restaurant.location_city.city, restaurant.location_state.state, restaurant.location_road.road, restaurant.location_detail)] 
+            })
+            # food
+            result.append({
+                "title"   : "음식 종류",
+                "content" : [restaurant.food.category]
+            })
+            # price
+            result.append({
+                "title"   : "가격대",
+                "content" : [restaurant.price_range.price_range]
+            })
+            # holiday
+            result.append({
+                "title"   : "휴일",
+                "content" : [restaurant.holiday.holiday] 
+            })
+            
+            info = restaurant.restaurant_info_set.values().get(restaurant_id = restaurant_id)
+            for el in info :
+                if el in title_dict and info[el] != None :
+                    result.append({"title" : title_dict[el], "content" : [info[el]]})
+
+            # menu
+            result.append({
+                "title"   : "메뉴",
+                "content" : [
+                    {
+                        "menu"  : el.menu,
+                        "price" : el.price 
+                    }
+                    for el in restaurant.menu_set.filter(restaurant_id=restaurant_id) ]
+            })
+
+            return JsonResponse({"result":result}, status = 200)
+        except Restaurant.DoesNotExist:
+            return HttpResponse(status = 404)
